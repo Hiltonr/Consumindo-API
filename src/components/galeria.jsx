5c821c3d3c53993c46436dfcfe2ecfabdf4d0984
Randys <hiltonrandysgss@hotmail.com>
2022-09-19 16:24:36 -0300

    commitfinal

--- src/components/galeria.jsx
+++ src/components/galeria.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 
 const Galeria = () => {
-  const [animal=[], setAnimal] = useState("oi");
+  const [animal, setAnimal] = useState([]);
 
   useEffect(() => {
     getAnimal();
   }, []);
 
   const getAnimal = async () => {
-    const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand/10", {
+    const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand/6", {
       method: "GET",
       
     });
@@ -16,140 +16,33 @@ const Galeria = () => {
     setAnimal(data);
     
   };
-  // Tentei o map, mas não funcionou. Sei a lógica, Porém  faltou tempo para buscar a solução
-  
+    
   return (
     <div className="container-animais">
-        
-      <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
+      {animal.map((ani) => {
+          
+          return (
+            <div className="container-animal">
+          
+          
+            
+            <div className="foto">
+          <img className="animais" src={ani.image_link}></img>
+            </div>
 
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div> 
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-        <div className="container-animal">
-          <div className="foto">
-        <img className="animais" src={animal[0].image_link}></img>
-          </div>
-          <p >Nome: {animal[0].name}</p>
-          <p >Dieta: {animal[0].diet}</p>
-          <p >Tipo: {animal[0].animal_type}</p>
-          <p >Latim: {animal[0].latin_name}</p>
-          <p >Habitar: {animal[0].habitat} </p>
-          <p >Localização: {animal[0].geo_range}</p>
-         
-        </div>
-
+            <p >Nome: {ani.name}</p>
+            <p >Dieta: {ani.diet}</p>
+            <p >Tipo: {ani.animal_type}</p>
+            <p >Latim: {ani.latin_name}</p>
+            <p >Habitar: {ani.habitat} </p>
+            <p >Localização: {ani.geo_range}</p>
+          
+            </div>
+      )}
+          )}
+           
+    
+  
 
 
         
